f3a767768bcb4de53d0f63fe22383c34091c24b7
Venkata <venkata@users.noreply.github.com>
2026-04-23 12:14:39 -0700

    stuff

--- PythonProjects/portfolio_mpt/src/paths.py
+++ PythonProjects/portfolio_mpt/src/paths.py
@@ -3,5 +3,5 @@ from pathlib import Path
 
 ROOT = Path(__file__).resolve().parent.parent
 
-IA_DATA = ROOT / "IA_Portfolio_Data.xlsx"
-DISSERTATION_DATA = ROOT / "Dissertation_Full_Data.xlsx"
+PORTFOLIO_DATA = ROOT / "portfolio_data.xlsx"
+MULTI_ASSET_DATA = ROOT / "multi_asset_data.xlsx"
